9d00bcb1efc65bfc5c788abed478783dcce00436
JoshuaBidjou123 <kristen.bidjou@gmail.com>
2025-09-13 12:52:01 -0400

    Commit

--- numberguessing.py
+++ numberguessing.py
@@ -2,16 +2,15 @@
 my_dict ={1: 'apple', 2: 'ball'}
 
 # dictionary with mixed keys
-my_dict ={'name': 'John', 1: [8, 10, 23]}
 
-my_dict ={'name': 'Jack', 'age': 26}
+my_dict ={'name': 'Jack', 'age': 12, 1: [8, 10, 23]}
 
 # Output: Jack
 print(my_dict['name'])
 print(my_dict.get('age'))
 
 # update value
-my_dict['age'] = 26
+my_dict['age'] = 12
 print(my_dict)
 
 # remove particular element
